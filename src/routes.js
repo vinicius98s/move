7e545b360ada328988414ffefabf00882c8ccbeb
Vinicius Sales <vinicius.2010.s@gmail.com>
2019-11-15 19:04:33 -0300

    add: Bottom tab pages

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,62 @@
+import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {Icon} from 'react-native-eva-icons';
 
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Wallet from './pages/Wallet';
 import FirstLogin from './pages/FirstLogin';
 
 import AuthLoading from './pages/AuthLoading';
 
-const AppNavigation = createBottomTabNavigator({Home});
+import theme from './assets/theme';
+
+const AppNavigation = createBottomTabNavigator(
+  {
+    Home: {
+      screen: Home,
+      navigationOptions: {
+        tabBarIcon: ({tintColor}) => (
+          <Icon name="home-outline" fill={tintColor} width="24" height="24" />
+        ),
+      },
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        tabBarIcon: ({tintColor}) => (
+          <Icon name="person-outline" fill={tintColor} width="24" height="24" />
+        ),
+      },
+    },
+    Wallet: {
+      screen: Wallet,
+      navigationOptions: {
+        tabBarIcon: ({tintColor}) => (
+          <Icon
+            name="credit-card-outline"
+            fill={tintColor}
+            width="24"
+            height="24"
+          />
+        ),
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: theme.colors.green,
+      showLabel: false,
+      style: {
+        backgroundColor: theme.colors.darkBlue,
+      },
+    },
+  },
+);
 
 const AuthStack = createStackNavigator(
   {
